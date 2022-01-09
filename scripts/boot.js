97b353c94cfa189100a759d41ebc02c4c4dd862b
roonaan <roonaan@gmail.com>
2022-01-09 18:50:51 +0100

    Add basic logic

--- scripts/boot.js
+++ scripts/boot.js
@@ -1,7 +1,31 @@
-function enhance(rootNode) {
+function start(rootNode) {
   const node = rootNode || document.getElementById('main');
   node.innerHTML = 'Bezig met laden';
+  
+  loadPage(node, 'homepage');
 }
 
+function enhance(rootNode) {
+  rootNode.style.border = "dashed 10px green";
+}
 
-document.body.addEventListener('onload', enhance);
+function loadPage(node, pagina) { 
+  node.style.border = "dotted 1px aqua";
+  get('pages/' + pagina + '.html', function(content) {
+    node.innerHTML = content;
+    enhance(node);
+  });
+}
+
+const http = {
+  get: function(url, onload) {
+    var client = new XMLHttpRequest();
+    client.open('GET', onload);
+    client.onreadystatechange = function() {
+      onload(client.responseText);
+    }
+    client.send();
+  }
+}
+
+document.body.addEventListener('onload', start);
