bd54743052bdc2817b73c385663e813885584e4a
roonaan <roonaan@gmail.com>
2022-01-09 20:43:57 +0100

    Update boot.js

--- scripts/boot.js
+++ scripts/boot.js
@@ -9,7 +9,7 @@ function start(rootNode) {
 function enhance(rootNode) {
   rootNode.style.border = "solid 1px green";
   onEvent(rootNode.querySelectorAll('[data-page]'), 'click', onPageChange);
-  widgets(rootNode.querySelectorAll('[data-dialoog]', Dialoog);
+  widgets(rootNode.querySelectorAll('[data-dialoog]', 'Dialoog');
 }
 
 function onEvent(nodes, event, handler) {
@@ -17,10 +17,26 @@ function onEvent(nodes, event, handler) {
     nodes[i].addEventListener(event, handler);
   }
 }
-function widgets(nodes, construct) {
+function widgets(nodes, module) {
   for(var i = 0, c = nodes.length; i < c; i++) {
-    new construct(nodes[i]);
+    widget(nodes[i], module);
   }
+}
+function widget(node, module) {
+  if (module in window) {
+      new window[module](node);
+  }
+  const scriptId = "extra-module-" + module;
+  if (document.getElementById(scriptId)) {
+      setTimeout(function() {
+        widget(node, module);
+      }, 100);
+  }
+  const script = document.createElement('script');
+  script.type = "text/javascript";
+  script.src = "scripts/" + script + ".js?" + new Date().getTime();
+  script.id = scriptId;
+  document.body.appendChild(script);
 }
 
 function onPageChange(event) {
