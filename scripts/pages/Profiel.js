2438ae58b8b9383dcf34e336d0d9d679e6ef2ca7
roonaan <roonaan@gmail.com>
2022-01-10 20:15:54 +0100

    Update Profiel.js

--- scripts/pages/Profiel.js
+++ scripts/pages/Profiel.js
@@ -1,7 +1,9 @@
 getModule('Inventory', function(Inventory) {
   
   function Pages_Profiel(node) {
-    (node.querySelector('[data-inventory]') || document.createElement('div')).innerHTML = JSON.stringify(Inventory.getItems());
+    const inventoryItems = Inventory.getItems();
+    console.log('Current inventory', inventoryItems);
+    (node.querySelector('[data-inventory]') || document.createElement('div')).innerHTML = JSON.stringify(inventoryItems);
   }
   
   window.Pages_Profiel = Pages_Profiel;
