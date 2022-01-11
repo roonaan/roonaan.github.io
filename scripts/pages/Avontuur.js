b0eabb2191aeb6c73757c4096e3a87900991cd19
roonaan <roonaan@gmail.com>
2022-01-11 20:04:02 +0100

    Update Avontuur.js

--- scripts/pages/Avontuur.js
+++ scripts/pages/Avontuur.js
@@ -1,6 +1,51 @@
 getModule('Storage', function(Storage) {
+    
     function Avontuur (node) {
-      node.innerHTML = 'Bezig met laden. Even geduld joh!';
+        this.node = node;
+        this.avontuurNode = node.querySelector('.avonturen');
+        if (!this.avontuurNode) {
+            node.innerHTML = 'Er is iets misgegaan';
+            return;
+        }
+        this.avontuurNode = H_LAAD_ICON;
+        const avontuur = this;
+        http.get('avonturen/overzicht.json?' + new Date().getTime(), function(text) {
+            avontuur.avonturen = JSON.parse(text);
+            avontuur.render('overzicht');
+        });
+    }
+    
+    Avontuur.prototype.render = function(pagina) {
+        const avontuur = this;
+        if (pagina === 'overzicht') {
+            this.avontuurNode.innerHTML = '';
+            Object.keys(this.avonturen).forEach(item => {
+                const button = document.createElement('button');
+                button.type = 'button';
+                button.className = 'start-button';
+                button.style.margin = '2%';
+                button.addEventListener('click', function() {
+                    notificatie('We gaan op ' + item + ' avontuur!');
+                    avontuur.render(item);
+                });
+                this.avontuurNode.appendChild(button);
+            });
+        }
+        if (item in this.avonturen) {
+            const items = this.avonturen[item];
+            items.unshift('overzicht');
+            items.forEach(item => {
+                const button = document.createElement('button');
+                button.type = 'button';
+                button.className = 'start-button';
+                button.style.margin = '2%';
+                button.addEventListener('click', function() {
+                    window.alert('We gaan op avontuur');
+                    avontuur.render(item);
+                });
+                this.avontuurNode.appendChild(button);
+            });
+        }
     }
   
     window.Pages_Avontuur = Avontuur;
