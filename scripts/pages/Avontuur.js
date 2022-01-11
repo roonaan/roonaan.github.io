4683fc0e6837cc87178309178cdaa3b5120708f3
roonaan <roonaan@gmail.com>
2022-01-11 19:50:15 +0100

    Create Avontuur.js

---
+++ scripts/pages/Avontuur.js
@@ -0,0 +1,7 @@
+getModule('Storage', function(Storage) {
+    function Avontuur (node) {
+      node.innerHTML = 'Bezig met laden. Even geduld joh!';
+    }
+  
+    window.Pages_Avontuur = Avontuur;
+});
