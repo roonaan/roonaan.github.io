769883aa279a4f0624120b2e84f489ad6815140b
roonaan <roonaan@gmail.com>
2022-01-12 17:37:14 +0100

    Update Profiel.js

--- scripts/pages/Profiel.js
+++ scripts/pages/Profiel.js
@@ -3,7 +3,31 @@ getModule('Inventory', function(Inventory) {
   function Pages_Profiel(node) {
     const inventoryItems = Inventory.getItems();
     console.log('Current inventory', inventoryItems);
-    (node.querySelector('[data-inventory]') || document.createElement('div')).innerHTML = JSON.stringify(inventoryItems);
+    this.inventoryNode = (node.querySelector('[data-inventory]') || document.createElement('div'));
+    this.inventoryNode.innerHTML = '';
+    Object.entries(inventoryItems).forEach(item => {
+      const key = item[0];
+      const aantal = item[1];
+      
+      const blok = document.createElement('div');
+      blok.className = 'inv-blok';
+      
+      const icoon = document.createElement('img');
+      icoon.alt = item;
+      icoon.className = 'inv-icoon';
+      blok.appendChild(icoon);
+      
+      const naam = document.createElement('div');
+      naam.className = 'inv-naam';
+      naam.innerText = key;
+      blok.appendChild(naam);
+      
+      const teller = document.createElement('div');
+      teller.className = 'inv-teller';
+      teller.innerText = aantal;
+      blok.appendChild(teller);
+      this.inventoryNode.appendChild(blok);
+    });
   }
   
   window.Pages_Profiel = Pages_Profiel;
