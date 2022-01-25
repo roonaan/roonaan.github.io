90d22eaa1cdd116c3ce63d563dcddf020738525f
roonaan <roonaan@gmail.com>
2022-01-25 16:25:08 +0100

    Reset pagina case sensitivity issue

--- pages/reset/Reset.js
+++ pages/reset/Reset.js
@@ -1,22 +1,25 @@
-function reset(node) {
-  const form = node.querySelector('form[name=reset]');
-  form.querySelector('button').addEventListener('click', function() {
-    if (form.elements.voortgang && form.elements.voortgang.checked) {
-      getModule('MissieVoortgang', function(mv) { mv.reset(); });
-      getModule('KarakterLijst', function(k) { k.reset(); });
-      notificatie('Het spel herstart in 5 seconden');
-      setTimeout(function() {
-          document.location.hash = '';
-          document.location.reload();
-      }, 5000);
-    }
-    if (form.elements.inventory && form.elements.inventory.checked) {
-      getModule('Inventory', function(i) { i.reset(); });
-    }
-    if (form.elements.cheats && form.elements.cheats.checked) {
-      getModule('Inventory', function(i) { i.addItem('honesty', 1); });
-    }
-  });
-}
+(function() {
 
-window.Pages_Reset = reset;
+  function Reset(node) {
+    const form = node.querySelector('form[name=reset]');
+    form.querySelector('button').addEventListener('click', function() {
+      if (form.elements.voortgang && form.elements.voortgang.checked) {
+        getModule('MissieVoortgang', function(mv) { mv.reset(); });
+        getModule('KarakterLijst', function(k) { k.reset(); });
+        notificatie('Het spel herstart in 5 seconden');
+        setTimeout(function() {
+            document.location.hash = '';
+            document.location.reload();
+        }, 5000);
+      }
+      if (form.elements.inventory && form.elements.inventory.checked) {
+        getModule('Inventory', function(i) { i.reset(); });
+      }
+      if (form.elements.cheats && form.elements.cheats.checked) {
+        getModule('Inventory', function(i) { i.addItem('honesty', 1); });
+      }
+    });
+  }
+
+  window.Pages_Reset = Reset;
+})();
