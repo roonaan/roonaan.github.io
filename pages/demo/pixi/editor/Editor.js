4de0daae762a0a873ba8d18622f2f33cbf2d3118
roonaan <roonaan@gmail.com>
2022-01-30 17:41:48 +0100

    Beloning voor gevecht 8

--- pages/demo/pixi/editor/Editor.js
+++ pages/demo/pixi/editor/Editor.js
@@ -13,6 +13,7 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
             this.previewNode = node.querySelector('[data-preview]');
             this.editorNode = node.querySelector('[data-editor]');
             this.spriteList = node.querySelector('[data-sprites]');
+            this.beloningNode = node.querySelector('[data-beloningen]');
 
             this.model = STORE.getItem('model');
             console.log('Latest model', this.model);
@@ -24,17 +25,45 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                 });
                 this.setTerrein('assets/terrein/opa-free-desert/opa-free-desert.json');
             }
-            this.editorTemplate = Handlebars.compile(node.querySelector('#editor-tpl').innerHTML);
+            if (!this.model.kaart.tileSize) {
+                this.model.kaart.tileSize = 250;
+                this.save();
+            }
+            this.editorTemplate = Handlebars.compile(node.querySelector('#tile-editor-tpl').innerHTML);
+            this.objectEditorTemplate = Handlebars.compile(node.querySelector('#object-editor-tpl').innerHTML);
+            this.beloningEditorTemplate = Handlebars.compile(node.querySelector('#beloning-editor-tpl').innerHTML);
+            this.beloningNode.innerHTML = this.beloningEditorTemplate({}).trim();
             this.reload();
+
+            node.querySelector('[data-laag]').addEventListener('change', () => {
+                editor.onLaagChange();
+            });
+            editor.onLaagChange(true);
 
             node.querySelector('[data-export]').addEventListener('click', () => {
                 document.addEventListener('copy', function(e) {
                     const tegels = editor.getTegels();
                     const terrein = editor.model.kaart.terrein;
+                    const objecten = editor.getObjecten();
+                    const vijanden = editor.getVijanden();
+                    const shared = ['kampvuur'];
+                    const tileSize = editor.model.kaart.tileSize || 250;
+                    const achtergrond = editor.model.kaart.achtergrond || 'Tile_5';
                     const data = {
-                        tegels, terrein
+                        kaart: {
+                            terrein,
+                            shared,
+                            tileSize,
+                            achtergrond,
+                            tegels,
+                            objecten
+                        },
+                        vijanden
                     };
-                    e.clipboardData.setData('text/plain', JSON.stringify(data, null, '  '));
+                    let json = JSON.stringify(data, null, '  ').trim();
+                    json = json.replace(/^\{/, '');
+                    json = json.replace(/}$/, ',');
+                    e.clipboardData.setData('text/plain', json);
                     e.preventDefault();
                   });
                   document.execCommand('copy');
@@ -59,7 +88,7 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                     backgroundAlpha: 0.25,
                     resizeTo: editor.previewNode
                 });
-                editor.tileSize = 80;
+                editor.displaySize = STORE.getItem('displaySize') || 80;
                 editor.pixi = app;
                 editor.previewNode.innerHTML = '';
                 editor.previewNode.appendChild(this.pixi.view);
@@ -82,6 +111,38 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                 }
                 return acc;
             }, []);;
+        }
+
+        getObjecten() {
+            return (this.model.kaart.objecten || []).filter(Boolean);
+        }
+
+        getVijanden() {
+            return (this.model.vijanden || []).filter(Boolean);
+        }
+
+        onLaagChange(skipRender = false) {
+            // Tijdens het opstarten kan het renderen fout gaan. Maar
+            // die code wordt later sowieso aangeroepen, dus het is geen
+            // probleem als we het nu overslaan.
+            if (!skipRender) {
+                this.render();
+            }
+            if (this.isObjecten()) {
+                this.renderObjectenEditor();
+            }
+        }
+
+        getLaag() {
+            return this.node.querySelector('[data-laag]').value;
+        }
+
+        isGrond() {
+            return this.getLaag() === 'grond';
+        }
+
+        isObjecten() {
+            return this.getLaag() === 'objecten';
         }
 
         save() {
@@ -129,7 +190,8 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
             inButton.style.left = '0px';
             inButton.style.bottom = '0%';
             inButton.addEventListener('click', () => {
-                this.tileSize += 10;
+                this.displaySize += 10;
+                STORE.setItem('displaySize', this.displaySize);
                 this.render();
             });
             const outButton = document.createElement('button');
@@ -138,7 +200,8 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
             outButton.style.left = '100px';
             outButton.style.bottom = '0%';
             outButton.addEventListener('click', () => {
-                this.tileSize = Math.max(10, this.tileSize - 10);
+                this.displaySize = Math.max(10, this.displaySize - 10);
+                STORE.setItem('displaySize', this.displaySize);
                 this.render();
             });
             this.previewNode.appendChild(leftBtn);
@@ -167,6 +230,10 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
         }
 
         onTileClicked(row, col) {
+            if (!this.isGrond()) {
+                console.log('We doen niets als we niet in de grond laag zitten');
+                return;
+            }
             const editor = this;
             const tile = this.model.kaart.tegels[row]?.[col] || '';
             const tegel = {
@@ -225,12 +292,14 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
 
         render() {
             const editor = this;
-            const tileSize = this.tileSize;
+            const displaySize = this.displaySize;
+            const tileSize = this.model.kaart.tileSize;
+            const scale = displaySize/tileSize;
             const box = this.box;
             let maxRows = 0;
-            let maxColumns = 0;
             box.removeChildren();
             const rows = this.getTegels();
+            let maxColumns = Math.max(rows.map(t => t.length));
             const lastRow = [];
             rows.push(lastRow);
             rows.forEach( (row, rowIndex) => {
@@ -244,34 +313,73 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                 }
                 row.forEach( (col, colIndex) => {
                     maxColumns = Math.max(maxColumns, colIndex);
-                    const gfx = new PIXI.Graphics();
-                    gfx.beginFill(0x0000FF, 0.1);
-                    gfx.lineStyle(1, 0x0000FF);
-                    gfx.x = colIndex * tileSize;
-                    gfx.y = rowIndex * tileSize;
-                    gfx.drawRect(0, 0, tileSize, tileSize);
-                    gfx.interactive = true;
-                    gfx.on('pointerdown', function() {
-                        editor.onTileClicked(rowIndex, colIndex);
-                    });
-                    box.addChild(gfx);
+                    if (this.isGrond()) {
+                        const gfx = new PIXI.Graphics();
+                        gfx.beginFill(0x0000FF, 0.1);
+                        gfx.lineStyle(1, 0x0000FF);
+                        gfx.x = colIndex * displaySize;
+                        gfx.y = rowIndex * displaySize;
+                        gfx.drawRect(0, 0, displaySize, displaySize);
+                        gfx.interactive = true;
+                        gfx.on('pointerdown', function() {
+                            editor.onTileClicked(rowIndex, colIndex);
+                        });
+                        box.addChild(gfx);
+                    }
                     const items = col.split(',');
                     items.forEach( (item) => {
                         const sprite = new PIXI.Sprite(this.resources.terrein.textures[item + ".png"]);
-                        sprite.x = colIndex * tileSize;
-                        sprite.y = rowIndex * tileSize;
-                        if (sprite.height > tileSize) {
-                            sprite.scale.y = 1 / sprite.height * tileSize;
-                        }
-                        if (sprite.width > tileSize) {
-                            sprite.scale.x = 1 / sprite.width * tileSize;
-                        }
-                        sprite.widht = tileSize;
+                        sprite.x = colIndex * displaySize;
+                        sprite.y = rowIndex * displaySize;
+                        sprite.scale.set(scale);
                         box.addChild(sprite);
                     });
                 });
             });
-            console.log( { tileSize, maxRows, maxColumns });
+            const objecten = this.getObjecten();
+            objecten.forEach( (item, itemIndex) => {
+                const sprite = new PIXI.Sprite(this.resources.terrein.textures[item.tegel]);
+                sprite.x = item.x * displaySize;
+                sprite.y = item.y * displaySize;
+                sprite.scale.set(scale);
+                if (editor.isObjecten()) {
+                    const text = new PIXI.Text(
+                        "(" + item.x + ", " + item.y + ") " + item.tegel,
+                        {
+                            fontFamily: "Arial",
+                            fontSize: 12,
+                            fill: 0x0000FF
+                        }
+                    );
+                    text.x = item.x * displaySize + 0.5 * (sprite.width - text.width);
+                    text.y = item.y * displaySize + sprite.height;
+                    box.addChild(text);
+                    const gfx = new PIXI.Graphics();
+                    gfx.beginFill(0xCCCCFF, 0.1);
+                    gfx.lineStyle(1, 0x0000FF);
+                    gfx.x = sprite.x;
+                    gfx.y = sprite.y;
+                    gfx.drawRect(0, 0, sprite.width, sprite.height);
+                    gfx.endFill();
+                    gfx.interactive = true;
+                    gfx.on('pointerdown', () => {
+                        const nodes = editor.editorNode.querySelectorAll('[data-object]');
+                        nodes.forEach((node) => {
+                            if (node.getAttribute('data-object') == itemIndex) {
+                                node.querySelector('input').focus();
+                                node.querySelector('input').scrollIntoView();
+                                const prev = node.style.backgroundColor;
+                                node.style.backgroundColor = 'blue';
+                                setTimeout(function() {
+                                    node.style.backgroundColor = prev;
+                                }, 1000);
+                            }
+                        })
+                    });
+                    box.addChild(gfx);
+                }
+                box.addChild(sprite);
+            });
         }
 
         renderSpriteList() {
@@ -342,7 +450,11 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                     gfx.interactive = true;
                     gfx.on('pointerdown', () => {
                         if (editor.lastClickedInput) {
-                            editor.lastClickedInput.value = safeName;
+                            if (editor.lastClickedInput.getAttribute('data-extension') == 'true') {
+                                editor.lastClickedInput.value = textName;
+                            } else {
+                                editor.lastClickedInput.value = safeName;
+                            }
                             editor.lastClickedInput.dispatchEvent(new CustomEvent('change', {bubbles: true}));
                         }
                     });
@@ -350,6 +462,133 @@ getModules('GameStorage', 'pixi/Engine', function(GameStorage) {
                     app.stage.addChild(sprite);
                     app.stage.addChild(label);
                 });
+            });
+        }
+
+        renderObjectenEditor() {
+            const editor = this;
+            const objecten = this.getObjecten().map( (item, index) => {
+                return {...item, index};
+            });
+            const nieuwIndex = objecten.length;
+            this.editorNode.innerHTML = this.objectEditorTemplate({
+                objecten,
+                nieuwIndex
+            }).trim();
+            this.editorNode.firstChild.addEventListener('click', (ev) => {
+                console.log('Handling click');
+                const action = ev.target.getAttribute('data-action');
+                if (action === 'select') {
+                    editor.lastClickedInput = ev.target;
+                } else if (action === 'beloningen') {
+                    const object = parseInt(ev.target.closest('[data-object]').getAttribute('data-object'));
+                    editor.renderBeloningenEditor(object);
+                } else {
+                    console.log('Unknown action', action);
+                }
+            });
+            this.editorNode.firstChild.addEventListener('change', (ev) => {
+                const point = ev.target.getAttribute('data-point');
+                const value = ev.target.value;
+                console.log('Handling change', point, value);
+                if (value && ['x', 'y', 'tegel'].includes(point)) {
+                    const object = parseInt(ev.target.closest('[data-object]').getAttribute('data-object'));
+                    editor.model.kaart.objecten = editor.model.kaart.objecten || [];
+                    editor.model.kaart.objecten[object] = editor.model.kaart.objecten[object] || {};
+                    if (['x', 'y'].includes(point)) {
+                        editor.model.kaart.objecten[object][point] = parseFloat(value);
+                    } else {
+                        editor.model.kaart.objecten[object][point] = value;
+                    }
+                    editor.save();
+                    if (object === nieuwIndex) {
+                        editor.renderObjectenEditor();
+                    }
+                    editor.render();
+                } else {
+                    console.log('Unknown data point [' + point + '] for value [' + value + ']');
+                }
+            });
+        }
+
+        renderBeloningenEditor(object) {
+            const item = this.getObjecten()[object];
+            if (!item) {
+                console.warn('Item bestaat niet', object);
+                return;
+            }
+
+            const data = {
+                x: item.x,
+                y: item.y,
+                tegel: item.tegel,
+                eenmalig: [],
+                altijd: []
+            };
+
+            if (item.beloning) {
+                if (item.beloning.eenmalig) {
+                    data.eenmalig = item.beloning.eenmalig.map( (item, index) => {
+                        return {
+                            index,
+                            wat: item[0] || '',
+                            hoeveel: item[1] || 1,
+                            kans: item[2] || 100
+                        }
+                    }, [])
+                }
+                if (item.beloning.altijd) {
+                    data.altijd = item.beloning.altijd.map( (item, index) => {
+                        return {
+                            index,
+                            wat: item[0] || '',
+                            hoeveel: item[1] || 1,
+                            kans: item[2] || 100
+                        }
+                    }, [])
+                }
+            }
+
+            data.nieuwEenmaligIndex = data.eenmalig.length;
+            data.nieuwAltijdIndex = data.altijd.length;
+
+            this.beloningNode.innerHTML = this.beloningEditorTemplate(data).trim();
+            this.beloningNode.style.display = "block";
+
+            const editor = this;
+            this.beloningNode.querySelector('[data-sluiten]').addEventListener('click', () => {
+                editor.beloningNode.style.display = "none";
+            });
+            this.beloningNode.addEventListener('change', (ev) => {
+                const modelData = editor.model.kaart.objecten[object] || {};
+                const eenmalig = ev.target.getAttribute('data-eenmalig');
+                if (eenmalig) {
+                    const beloning = modelData.beloning = modelData.beloning || {};
+                    const modelEenmalig = beloning.eenmalig = beloning.eenmalig || [];
+                    const parts = eenmalig.split(',');
+                    const index = parseInt(parts[0]);
+                    const field = parseInt(parts[1]);
+                    modelEenmalig[index] = modelEenmalig[index] || [];
+                    modelEenmalig[index][field] = ev.target.value;
+                    editor.save();
+                    if (index === data.nieuwEenmaligIndex) {
+                        editor.renderBeloningenEditor(object);
+                    }
+                }
+                const altijd = ev.target.getAttribute('data-altijd');
+                if (altijd) {
+                    const beloning = modelData.beloning = modelData.beloning || {};
+                    const modelAltijd = beloning.altijd = beloning.altijd || [];
+                    const parts = altijd.split(',');
+                    const index = parseInt(parts[0]);
+                    const field = parseInt(parts[1]);
+                    modelAltijd[index] = modelAltijd[index] || [];
+                    modelAltijd[index][field] = ev.target.value;
+                    editor.save();
+                    if (index === data.nieuwAltijdIndex) {
+                        editor.renderBeloningenEditor(object);
+                    }
+                }
             });
         }
 
